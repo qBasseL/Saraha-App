2bed0380649ead421d27b3fd2303903e9b009190
Bassel <basselalaa17@gmail.com>
2026-03-11 22:35:03 +0200

    Gmail Auth

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { successResponse } from "../../common/utils/index.js";
 import { getUser, rotateToken } from "./user.service.js";
+import { authenticate } from "../../middleware/authentication.middleware.js";
+import { TokenTypeEnums } from "../../common/enums/security.enum.js";
 
 const router = Router();
 
-router.get('/', async(req, res, next) => {
-    const result = await getUser(req.headers.authorization);
+router.get('/', authenticate(), async(req, res, next) => {
+    const result = await getUser(req.user);
     return successResponse({res, status: 200, data: result})
 })
 
-router.post('/token-rotate', async(req, res, next) => {
-    const result = await rotateToken(req.headers.authorization,);
+router.post('/token-rotate', authenticate(TokenTypeEnums.Refresh), async(req, res, next) => {
+    const result = await rotateToken(req.user);
     return successResponse({res, status: 201, data: result})
 })
 
